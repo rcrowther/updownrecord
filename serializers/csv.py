95a46f4bb2c0abd0a38b4ab595f79c3f729b7347
rcrowther <rw.crowther@gmail.com>
2018-03-09 14:59:04 +0000

    Init instructions initialisation should conform to Serialisation interface

--- serializers/csv.py
+++ serializers/csv.py
@@ -2,16 +2,20 @@ import csv
 
 
 from django.db import DEFAULT_DB_ALIAS
-#from django.core.serializers import base
 from .nonrelational_python import NonrelationalSerializer, NonrelationalDeserializer
+from django.core.exceptions import ImproperlyConfigured
 
 
 class Serializer(NonrelationalSerializer):
     dialect='excel'
-
-    def serialize(self, queryset, model_class, *args, stream=None, fields=None,
+    model_class = None
+        
+    def serialize(self, queryset, *args, stream=None, fields=None, model_class=None,
                   use_natural_primary_keys=False, progress_output=None, object_count=0, **options):
-        self.model_class = model_class
+        if (model_class is not None): 
+            self.model_class = model_class
+        if (self.model_class is None): 
+            raise ImproperlyConfigured('Must have a "model_class" attibute.')  
         #! test is model
         return super().serialize(queryset, *args, stream=stream, fields=fields,
                   use_natural_primary_keys=use_natural_primary_keys, progress_output=progress_output, object_count=object_count, **options)
@@ -20,8 +24,6 @@ class Serializer(NonrelationalSerializer):
         """
         Start serialization -- open the XML document and the root element.
         """
-        #self.obj_dict = {} 
-        #self.model_class = model_class
         fieldnames = ['pk']
         fieldnames.extend(self.field_names(self.model_class))
         self.writer = csv.DictWriter(self.stream, fieldnames=fieldnames, dialect=self.dialect)
@@ -29,12 +31,21 @@ class Serializer(NonrelationalSerializer):
 
     def end_serialization(self):
         pass
-        
-    #def start_object(self, obj):
-    #    pass
-          
+
+    def verify_object_from_model_class(self, model_class, obj):
+        if not (obj._meta.model == model_class):
+            raise base.SerializationError("Object of class {} given to a serialiser handling class {}".format( 
+            type(obj), 
+            model_class.model_name
+            ))
+
+    def start_object(self, obj):
+        self.verify_object_from_model_class(self.model_class, obj)
+        super().start_object(obj)
+
     def end_object(self, obj):
-        #test object is the model_class
+        # test object is the model_class
+        model_path
         d = self.get_dump_object(obj)
         obj_dict = d['fields']
         obj_dict['pk'] = d['pk']
@@ -44,41 +55,18 @@ class Serializer(NonrelationalSerializer):
         # Grandparent super
         return super(NonrelationalSerializer, self).getvalue()
         
-#class Deserializer(SingleModelKeyMapDeserializer):
-    #dialect = 'excel'
-    #has_header = True
-    
-    #def __init__(self, stream_or_string, model_class, dialect=None, has_header=True, key_map=None, pop_false=True,*args, **options):
-        #super().__init__(stream_or_string, model_class, *args, key_map=key_map, pop_false=pop_false, **options)
-        #if (dialect):
-            #self.dialect = dialect
-        #if (has_header):
-            #self.has_header = has_header
 
-        #if(self.has_header):
-            #self.reader = csv.DictReader(self.stream, dialect=self.dialect)
-        #else:
-            #field_names = self.local_field_names()
-            #field_names.append('pk')
-            #self.reader = csv.DictReader(self.stream, fieldnames=field_names, dialect=self.dialect)
-
-    #def __next__(self):
-        #for row in self.reader:
-            #return self._handle_object(row)
-        #raise StopIteration
-
-    #def _handle_object(self, row):
-        ##mrow = {self.map_key(k) : v for k, v in row.items()}
-        #mrow = {self.map_key(k) : v for k, v in row.items() if (v or (not self.pop_false))}
-        #obj = self.model_class(**mrow)
-        #return base.DeserializedObject(obj)
 
 class Deserializer(NonrelationalDeserializer):
     dialect = 'excel'
     has_header = True
+    model_class = None
     
-    def __init__(self, stream_or_string, model_class, *args, using=DEFAULT_DB_ALIAS, ignorenonexistent=False, dialect=False, has_header=True, **options):
-        self.model_class = model_class
+    def __init__(self, stream_or_string, *args, using=DEFAULT_DB_ALIAS, model_class=None, ignorenonexistent=False, dialect=False, has_header=True, **options):
+        if (model_class is not None): 
+            self.model_class = model_class
+        if (self.model_class is None): 
+            raise ImproperlyConfigured('Must have a "model_class" attibute.')        
         self.model_path = str(self.model_class._meta)
         if (dialect):
             self.dialect = dialect
